2ad5cf5264da8011c33dbd54ec3dfb64a6b799ef
tnw513 <tnw513@gmail.com>
2015-09-07 13:18:09 +0200

    Prima versione agente per lo scambio di messaggi con arduino. NON FUNZIONANTE.

--- domHome/src/serialCommAgent.java
+++ domHome/src/serialCommAgent.java
@@ -1,4 +1,42 @@
+import jade.core.Agent;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
 
-public class serialCommAgent {
+public class serialCommAgent extends Agent {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 138736042772986486L;
+
+protected void setup() {
+		
+		DFAgentDescription dfd = new DFAgentDescription();
+		dfd.setName(getAID());
+		ServiceDescription sd = new ServiceDescription();
+		sd.setType("serialcomm-manager");
+		sd.setName("JADE-serialcomm");
+		dfd.addServices(sd);
+		try {
+			DFService.register(this, dfd);
+		}
+		catch(FIPAException fe) {
+			fe.printStackTrace();
+		}
+		//addBehaviour(new Behaviour1());
+		//addBehaviour(new Behaviour1bis());
+	}
+	
+	protected void takeDown() {
+		// Deregister from the yellow pages
+		try {
+			DFService.deregister(this);
+		}
+		catch (FIPAException fe) {
+			fe.printStackTrace();
+		}
+		System.out.println("SerialCommAgent "+getAID().getName()+" terminating.");
+	}
 }
