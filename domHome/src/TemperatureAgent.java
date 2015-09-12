df641ef9ac4d97f48465571b70a2178114893954
tnw513 <tnw513@gmail.com>
2015-09-12 11:04:10 +0200

    Prima versione. Controlla solamente i RoomAgent registrati al DF

--- domHome/src/TemperatureAgent.java
+++ domHome/src/TemperatureAgent.java
@@ -1,4 +1,7 @@
+import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.TickerBehaviour;
 import jade.domain.DFService;
 import jade.domain.FIPAException;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
@@ -10,6 +13,7 @@ public class TemperatureAgent extends Agent {
 	 * 
 	 */
 	private static final long serialVersionUID = 138736042772986486L;
+	private AID[] serverAgents;
 
 	protected void setup() {
 
@@ -25,10 +29,43 @@ public class TemperatureAgent extends Agent {
 		catch(FIPAException fe) {
 			fe.printStackTrace();
 		}
-		//addBehaviour(new Behaviour1());
+		addBehaviour(new RequestCurrentTemperature(this,20000));
 		//addBehaviour(new Behaviour1bis());
 	}
+	
+	private class RequestCurrentTemperature extends TickerBehaviour {
+	
+	public RequestCurrentTemperature(Agent a, long period) {
+			super(a, period);
+			// TODO Auto-generated constructor stub
+		}
 
+	/**
+		 * 
+		 */
+		private static final long serialVersionUID = -7544895415079804828L;
+
+	@Override
+	protected void onTick() {
+		DFAgentDescription template = new DFAgentDescription();
+		ServiceDescription sd = new ServiceDescription();
+		sd.setType("room-manager");
+		template.addServices(sd);
+		try {
+			DFAgentDescription[] result = DFService.search(myAgent, template); 
+			System.out.println("Found the following seller agents:");
+			serverAgents = new AID[result.length];
+			for (int i = 0; i < result.length; ++i) {
+				serverAgents[i] = result[i].getName();
+				System.out.println(serverAgents[i].getName());
+			}
+		}
+		catch (FIPAException fe) {
+			fe.printStackTrace();
+		}
+		
+	}
+	}
 	protected void takeDown() {
 		// Deregister from the yellow pages
 		try {
