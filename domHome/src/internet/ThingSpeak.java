b840a2a2df08e2db08790db3d569841cf2ebc589
tnw513 <tnw513@gmail.com>
2015-10-05 19:49:00 +0200

    Ora permette l'esecuzione del programma anche senza connessione ad internet. Gestite le eccezioni se si è offline.

--- domHome/src/internet/ThingSpeak.java
+++ domHome/src/internet/ThingSpeak.java
@@ -1,22 +1,47 @@
 package internet;
+
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.SocketException;
 import java.net.URL;
+import java.net.UnknownHostException;
 
 public class ThingSpeak {
 
-   public static String getHTML(String urlToRead) throws Exception {
-      StringBuilder result = new StringBuilder();
-      URL url = new URL(urlToRead);
-      HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-      conn.setRequestMethod("GET");
-      BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-      String line;
-      while ((line = rd.readLine()) != null) {
-         result.append(line);
-      }
-      rd.close();
-      return result.toString();
-   }
+    public static String getHTML(String urlToRead) throws Exception {
+        /*   
+         Socket socket = null;
+         boolean reachable = false;
+         try {
+         socket = new Socket("thingspeak.com", 80);
+         reachable = true;
+         } finally {            
+         if (socket != null) try { socket.close(); } catch(IOException e) {}
+         }
+         System.out.println(reachable);
+         */
+        StringBuilder result = new StringBuilder();
+        URL url = new URL(urlToRead);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        BufferedReader rd = null;
+        try {
+            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line;
+            while ((line = rd.readLine()) != null) {
+                result.append(line);
+            }
+            rd.close();
+        } catch (UnknownHostException e) {
+            System.out.println("Impossibile inviare dati a thingspeak.");
+        }
+        catch (SocketException e) {
+            System.out.println("Impossibile inviare dati a thingspeak.");
+        }
+        return result.toString();
+    }
 }
