d18f615b09d1e2a12d00f1e1c35e51d936fefea1
tnw513 <tnw513@gmail.com>
2015-10-17 19:18:56 +0200

    Rimossi behaviour obsoleti e aggiunta registrazione al DF seguendo le convenzioni adottate per gli altri agenti.

--- domHome/src/temperature/ThermometerAgent.java
+++ domHome/src/temperature/ThermometerAgent.java
@@ -22,218 +22,191 @@ import jade.proto.AchieveREInitiator;
 import jade.proto.AchieveREResponder;
 
 public class ThermometerAgent extends Agent {
-	private Float currentTemperature;
-	String temperatureReplyWith = "temperature";
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -8355663085154105053L;
+    private Float currentTemperature;
+    String temperatureReplyWith = "temperature";
 
-	protected void setup() {
+    /**
+     *
+     */
+    private static final long serialVersionUID = -8355663085154105053L;
 
-		DFAgentDescription dfd = new DFAgentDescription();
-		dfd.setName(getAID());
-		ServiceDescription sd = new ServiceDescription();
-		sd.setType("thermometer-manager");
-		sd.setName("JADE-thermometer");
-		dfd.addServices(sd);
-		try {
-			DFService.register(this, dfd);
-		}
-		catch(FIPAException fe) {
-			fe.printStackTrace();
-		}
+    protected void setup() {
+        Object[] inRoom = this.getArguments();
+        String roomName = inRoom[0].toString();
+
+        DFAgentDescription dfd = new DFAgentDescription();
+        dfd.setName(getAID());
+        ServiceDescription sd = new ServiceDescription();
+        sd.setType("thermometer-manager");
+        sd.setName(roomName + "-thermometer");
+        dfd.addServices(sd);
+        try {
+            DFService.register(this, dfd);
+        } catch (FIPAException fe) {
+            fe.printStackTrace();
+        }
 		//addBehaviour(new RequestCurrentTemperature(this, 3000));
-		//addBehaviour(new GetCurrentTemperature());
-		addBehaviour(new GetCurrentTemperatureFIPA(this, 3000));
-		addBehaviour(new TempService());
-	}
+        //addBehaviour(new GetCurrentTemperature());
+        addBehaviour(new GetCurrentTemperatureFIPA(this, 3000));
+        addBehaviour(new TempService());
+    }
 
-	protected void takeDown() {
-		// Deregister from the yellow pages
-		try {
-			DFService.deregister(this);
-		}
-		catch (FIPAException fe) {
-			fe.printStackTrace();
-		}
-		System.out.println("ThermometerAgent "+getAID().getName()+" terminating.");
-	}
+    protected void takeDown() {
+        // Deregister from the yellow pages
+        try {
+            DFService.deregister(this);
+        } catch (FIPAException fe) {
+            fe.printStackTrace();
+        }
+        System.out.println("ThermometerAgent " + getAID().getName() + " terminating.");
+    }
 
-	private class TempService extends OneShotBehaviour {
+    private class TempService extends OneShotBehaviour {
 
+        /**
+         *
+         */
+        private static final long serialVersionUID = 6894891571072136375L;
 
-		/**
-		 * 
-		 */
-		private static final long serialVersionUID = 6894891571072136375L;
+        @Override
+        public void action() {
 
-		@Override
-		public void action() {
+            MessageTemplate template = MessageTemplate.and(
+                    MessageTemplate.MatchProtocol(FIPANames.InteractionProtocol.FIPA_REQUEST),
+                    MessageTemplate.MatchPerformative(ACLMessage.REQUEST));
 
-			MessageTemplate template = MessageTemplate.and(
-					MessageTemplate.MatchProtocol(FIPANames.InteractionProtocol.FIPA_REQUEST),
-					MessageTemplate.MatchPerformative(ACLMessage.REQUEST) );
+            addBehaviour(new AchieveREResponder(myAgent, template) {
 
-			addBehaviour(new AchieveREResponder(myAgent, template) {
+                /**
+                 *
+                 */
+                private static final long serialVersionUID = -694158247143718355L;
 
+                protected ACLMessage prepareResponse(ACLMessage request) throws NotUnderstoodException, RefuseException {
+                    if (request.getContent().equalsIgnoreCase("temperatura")) {
+                        // We agree to perform the action.
+                        ACLMessage agree = request.createReply();
+                        agree.setPerformative(ACLMessage.AGREE);
+                        return agree;
+                    } else {
+                        // We refuse to perform the action
+                        throw new RefuseException("Message content not supported");
+                    }
+                }
 
-				/**
-				 * 
-				 */
-				private static final long serialVersionUID = -694158247143718355L;
+                protected ACLMessage prepareResultNotification(ACLMessage request, ACLMessage response) throws FailureException {
+                    ACLMessage inform = request.createReply();
+                    inform.setPerformative(ACLMessage.INFORM);
 
-				protected ACLMessage prepareResponse(ACLMessage request) throws NotUnderstoodException, RefuseException {
-					if (request.getContent().equalsIgnoreCase("temperatura")) {
-						// We agree to perform the action.
-						ACLMessage agree = request.createReply();
-						agree.setPerformative(ACLMessage.AGREE);
-						return agree;
-					}
-					else {
-						// We refuse to perform the action
-						throw new RefuseException("Message content not supported");
-					}
-				}
+                    inform.setContent(currentTemperature.toString());
 
-				protected ACLMessage prepareResultNotification(ACLMessage request, ACLMessage response) throws FailureException {
-					ACLMessage inform = request.createReply();
-					inform.setPerformative(ACLMessage.INFORM);
+                    return inform;
 
-					inform.setContent(currentTemperature.toString());
+                }
+            });
 
-					return inform;
+        }
 
-				}
-			} );
+    }
 
-		}
+    private class GetCurrentTemperatureFIPA extends TickerBehaviour {
 
-	}
+        public GetCurrentTemperatureFIPA(Agent a, long period) {
+            super(a, period);
+            // TODO Auto-generated constructor stub
+        }
 
-	private class RequestCurrentTemperature extends TickerBehaviour {
+        /**
+         *
+         */
+        private static final long serialVersionUID = -7683060797864759807L;
 
-		public RequestCurrentTemperature(Agent a, long period) {
-			super(a, period);
-			// TODO Auto-generated constructor stub
-		}
-		/**
-		 * 
-		 */
-		private static final long serialVersionUID = 9072626078728707911L;
+        @Override
+        public void onTick() {
 
-		@Override
-		public void onTick() {
+            ACLMessage requestTemperatureMessage = new ACLMessage(ACLMessage.REQUEST);
 
-			AID msgReceiver= new AID("Gestore-Seriale",AID.ISLOCALNAME);
+            requestTemperatureMessage.setProtocol(FIPANames.InteractionProtocol.FIPA_REQUEST);
+            // We want to receive a reply in 10 secs
+            requestTemperatureMessage.setReplyByDate(new Date(System.currentTimeMillis() + 10000));
+            requestTemperatureMessage.setContent("therm1\n");
+            requestTemperatureMessage.addReceiver(new AID("Gestore-Seriale", AID.ISLOCALNAME));
 
-			ACLMessage serialAnswer = new ACLMessage(ACLMessage.REQUEST);
-			serialAnswer.setReplyWith(temperatureReplyWith);
-			serialAnswer.addReceiver(msgReceiver);
-			serialAnswer.setContent("therm1\n");
+            addBehaviour(new AchieveREInitiator(myAgent, requestTemperatureMessage) {
 
-			myAgent.send(serialAnswer);
+                /**
+                 *
+                 */
+                private static final long serialVersionUID = -5261992017174744491L;
 
-		}
-	}
-	
-	private class GetCurrentTemperatureFIPA extends TickerBehaviour {
+                protected void handleInform(ACLMessage inform) {
+                    String messageContenut = inform.getContent();
+                    if (messageContenut != null) {
+                        try {
+                            currentTemperature = Float.parseFloat(messageContenut);
+                        } catch (NumberFormatException e) {
+                            System.out.println("AgenteTermometro::::errore");
+                        }
+                    }
+                }
 
+                protected void handleRefuse(ACLMessage refuse) {
+                    System.out.println("Agent " + refuse.getSender().getName() + " refused to perform the requested action");
+                }
 
-		public GetCurrentTemperatureFIPA(Agent a, long period) {
-			super(a, period);
-			// TODO Auto-generated constructor stub
-		}
-
-		/**
-		 * 
-		 */
-		private static final long serialVersionUID = -7683060797864759807L;
-
-		@Override
-		public void onTick() {
-			
-			ACLMessage requestTemperatureMessage = new ACLMessage(ACLMessage.REQUEST);
-
-			requestTemperatureMessage.setProtocol(FIPANames.InteractionProtocol.FIPA_REQUEST);
-			// We want to receive a reply in 10 secs
-			requestTemperatureMessage.setReplyByDate(new Date(System.currentTimeMillis() + 10000));
-			requestTemperatureMessage.setContent("therm1\n");
-			requestTemperatureMessage.addReceiver(new AID("Gestore-Seriale",AID.ISLOCALNAME));
-
-			addBehaviour(new AchieveREInitiator(myAgent, requestTemperatureMessage) {
-
-				/**
-				 * 
-				 */
-				private static final long serialVersionUID = -5261992017174744491L;
-				protected void handleInform(ACLMessage inform) {
-					String messageContenut=inform.getContent();
-					if (messageContenut!=null) {
-						try {
-							currentTemperature = Float.parseFloat(messageContenut);
-						} catch (NumberFormatException e) {
-							System.out.println("AgenteTermometro::::errore");
-						}
-					}
-				}
-				protected void handleRefuse(ACLMessage refuse) {
-					System.out.println("Agent "+refuse.getSender().getName()+" refused to perform the requested action");
-				}
-				protected void handleFailure(ACLMessage failure) {
-					if (failure.getSender().equals(myAgent.getAMS())) {
+                protected void handleFailure(ACLMessage failure) {
+                    if (failure.getSender().equals(myAgent.getAMS())) {
 						// FAILURE notification from the JADE runtime: the receiver
-						// does not exist
-						System.out.println("Responder does not exist");
-					}
-					else {
-						System.out.println("Agent "+failure.getSender().getName()+" failed to perform the requested action");
-					}
-				}
-				protected void handleAllResultNotifications(Vector notifications) {
+                        // does not exist
+                        System.out.println("Responder does not exist");
+                    } else {
+                        System.out.println("Agent " + failure.getSender().getName() + " failed to perform the requested action");
+                    }
+                }
+
+                protected void handleAllResultNotifications(Vector notifications) {
 					//if (notifications.size() < nResponders) {
-					// Some responder didn't reply within the specified timeout
-					//System.out.println("Timeout expired: missing "+(nResponders - notifications.size())+" responses");
-					//}
-				}
-			} );
-			
-		}
-	}
+                    // Some responder didn't reply within the specified timeout
+                    //System.out.println("Timeout expired: missing "+(nResponders - notifications.size())+" responses");
+                    //}
+                }
+            });
 
-	private class GetCurrentTemperature extends CyclicBehaviour {
+        }
+    }
 
-		/**
-		 * 
-		 */
-		private static final long serialVersionUID = -2794051229003161225L;
+    private class GetCurrentTemperature extends CyclicBehaviour {
 
-		@Override
-		public void action() {
-			MessageTemplate mt1 = MessageTemplate.MatchPerformative(ACLMessage.INFORM);
-			MessageTemplate mt2 = MessageTemplate.MatchInReplyTo(temperatureReplyWith);
-			MessageTemplate mt = MessageTemplate.and(mt1, mt2);
-			//System.out.println("Server behaviour 1 wait a message.");
-			ACLMessage msg = myAgent.receive(mt);
-			if (msg!=null) {
+        /**
+         *
+         */
+        private static final long serialVersionUID = -2794051229003161225L;
 
-				String messageContenut=msg.getContent();
-				//System.out.println("AgenteTermometro::::"+messageContenut);
-				if (messageContenut!=null)
-					try {
-						currentTemperature = Float.parseFloat(messageContenut);
-					} catch (NumberFormatException e) {
-						System.out.println("AgenteTermometro::::errore");
-					}
+        @Override
+        public void action() {
+            MessageTemplate mt1 = MessageTemplate.MatchPerformative(ACLMessage.INFORM);
+            MessageTemplate mt2 = MessageTemplate.MatchInReplyTo(temperatureReplyWith);
+            MessageTemplate mt = MessageTemplate.and(mt1, mt2);
+            //System.out.println("Server behaviour 1 wait a message.");
+            ACLMessage msg = myAgent.receive(mt);
+            if (msg != null) {
 
-			}
-			else {
-				block();
-			}
+                String messageContenut = msg.getContent();
+                //System.out.println("AgenteTermometro::::"+messageContenut);
+                if (messageContenut != null) {
+                    try {
+                        currentTemperature = Float.parseFloat(messageContenut);
+                    } catch (NumberFormatException e) {
+                        System.out.println("AgenteTermometro::::errore");
+                    }
+                }
 
-		}
+            } else {
+                block();
+            }
 
-	}
+        }
+
+    }
 }
-
