f1814b854f6e97e17beeed79bd13f7a0710b7312
Silvio Didonna <tnw513@gmail.com>
2016-02-12 12:03:04 +0100

    inizio supporto gradle

--- src/main/java/Interface.java
+++ src/main/java/Interface.java
@@ -1,9 +1,6 @@
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Date;
-
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
